84028c38dbde0961a321931a97655b235610d1ef
linnjoha <linn.wallin@hotmail.com>
2024-05-24 10:22:21 +0200

    package json

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
-
+const __dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -18,10 +18,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// app.get("/", (req, res) => {
-//   //http://localhost:5000
-//   res.send("hello world");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDb();
